8e6bd501f402e926614547333fa3626a9165d5f3
Laura <you@example.com>
2020-02-17 19:16:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import Contact from './Contact';
+import Home from './Home';
+import About from './About';
+import Navigator from './Navigator';
+import"bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>React router</h1>
+     <BrowserRouter>
+      <div>
+        <Navigator />
+        <Switch>
+          <Route exact path = "/" component = {Home}/>
+          <Route path = "/contact" component = {Contact}/>
+          <Route path = "/about" component = {About}/>
+          <Route path = "/links" render = {() => <h1>Links</h1>}/>
+          <Route render = {() => <h1>Page not found</h1>}/>
+        </Switch>
+      </div>
+      </BrowserRouter>
+      
     </div>
   );
 }
